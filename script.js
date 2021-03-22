489bb7f7199024c350ab008e88ec0b1cc5a8907f
roody exantus <roodyprog@gmail.com>
2021-03-22 18:15:24 +0100

    mise en place des buttons"

--- script.js
+++ script.js
@@ -22,22 +22,27 @@ $(function() {
     function bindEvents (){
         $mainMenuItems.children('.images').click(function(){
             
-            let $newIndex = $(this).parent().index();//je recupere le parent et son index
-            let $item = $mainMenuItems.eq($newIndex);//retourne l'item que j'ai click
-            
-            if($openedIndex === $newIndex){
-                animateItem($item,false,250);
-                $openedIndex = -1;
-            }else{
-                if(validIndex($newIndex)){
-                    //close item open
-                    animateItem($mainMenuItems.eq($openedIndex),false,250);
-                    animateItem($item,true,250);
-                    $openedIndex = $newIndex;
-                }  
-                
-            }
+            let newIndex = $(this).parent().index();//je recupere le parent et son index
+            checkAnimateItem(newIndex);
         });
+
+        // events sur class button
+        $(".button").hover(/* class hover */
+            function() {
+                $(this).addClass("hovered");//ajoute la class hovered sur le button
+            },
+            function () {
+                $(this).removeClass("hovered");//suppr la class
+            }
+        );
+
+        // click button
+        $(".button").click(function() {
+            let newIndex = $(this).index();
+            checkAnimateItem(newIndex);
+        });
+
+
     }
 
 
@@ -49,6 +54,22 @@ $(function() {
         
         $colorImage.animate($colorImageParametre,$speed);//je modifie la position left avec un animation
         $item.animate($itemParametre,$speed)//open description
+    }
+
+    function checkAnimateItem($indexToCheckAndAnimate) {
+            
+            if($openedIndex === $indexToCheckAndAnimate){
+                animateItem($mainMenuItems.eq($indexToCheckAndAnimate),false,250);
+                $openedIndex = -1;
+            }else{
+                if(validIndex($indexToCheckAndAnimate)){
+                    //close item open
+                    animateItem($mainMenuItems.eq($openedIndex),false,250);
+                    animateItem($mainMenuItems.eq($indexToCheckAndAnimate),true,250);
+                    $openedIndex = $indexToCheckAndAnimate;
+                }  
+                
+            }
     }
 
     init();
